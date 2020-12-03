34089ed51fd7d5783a7c90443a36c7ded65e97f0
s4mking <samuel.simonney@golem.ai>
2020-12-03 17:41:01 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+window.ABLAST = {};
+
+// Assets managment
+require('./a-asset-image.js')
+
+require('./lib/utils.js');
+
+// Systems.
+
+// // Bullets.
+// require('./bullets/player.js');
+// require('./bullets/enemy-slow.js');
+// require('./bullets/enemy-medium.js');
+
+// Enemies.
+// require('./enemies/enemy_start.js');
+// require('./enemies/enemy0.js');
+
+// // Components
+// require('./components/proxy_event.js');
+// require('./components/decals.js');
+// require('./components/curve-movement.js');
+// require('./components/collision-helper.js');
+// require('./components/gamestate.js');
+// require('./components/gamestate-debug.js');
+// require('./components/shoot-controls.js');
+// require('./components/bullet.js');
+// require('./components/enemy.js');
+// require('./components/headset.js');
+// require('./components/json-model.js');
+// require('./components/spline-line.js');
+// require('./components/explosion.js');
+// require('./components/animate-message.js');
+// require('./components/sound-fade.js');
+// require('./components/restrict-position.js');
